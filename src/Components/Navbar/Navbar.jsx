4b0eadcdc07d9d56e3f9493052494880e0979ba7
mandor <hamzabrezy4@gmail.com>
2024-05-19 05:47:22 +0100

    v 0.3

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import logoImg from "../../images/icons8-books-48.png";
+import {HiOutlineMenuAlt3} from "react-icons/hi";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleNavbar = () => setToggleMenu(!toggleMenu);
+
+  return (
+    <nav className='navbar' id = "navbar">
+      <div className='container navbar-content flex'>
+        <div className='brand-and-toggler flex flex-sb'>
+          <Link to = "/" className='navbar-brand flex'>
+            <img src = {logoImg} alt = "site logo" />
+            <span className='text-uppercase fw-7 fs-16 ls-1'>GOLDENBOOK</span>
+
+          </Link>
+          <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
+            <HiOutlineMenuAlt3 size = {35} style = {{
+              color: `${toggleMenu ? "#fff" : "#8B4513"}`
+            }} />
+          </button>
+        </div>
+
+        <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
+          <ul className = "navbar-nav">
+            <li className='nav-item'>
+              <Link to="book" className='nav-link text-uppercase text-white fs-22 fw-bold ls-1'>Home</Link>
+
+            </li>
+            <li className='nav-item'>
+              <Link to = "about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>about</Link>
+          
+            </li>
+            <li className='nav-item'>
+              <Link to = "login" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>Login</Link>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import { AppProvider } from './context.';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./Components/BookList/BookList";
+import BookDetails from "./Components/BookDetails/BookDetails";
+import Login from "./Components/Login-Register/login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<BookList />} />
+          <Route path="book/:id" element={<BookDetails />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Router>
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
