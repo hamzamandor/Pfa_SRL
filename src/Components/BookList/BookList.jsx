1f0099d9606c8d7d09a89015fadaeb941f16fead
hamza mandor <hamzabrezy@gmail.com>
2024-06-20 15:22:42 +0100

    v4.1

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,43 +1,53 @@
 import React from 'react';
-import { useGlobalContext } from '../../context.';
-import Book from "../BookList/Book";
-import Loading from "../Loader/Loader";
-import coverImg from "../../images/cover_not_found.jpg";
-import "./BookList.css";
+import { useGlobalContext } from '../../context.'; // Assurez-vous que ce chemin est correct
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import Book from '../BookList/Book';
+import Loading from '../Loader/Loader';
+import coverImg from '../../images/cover_not_found.jpg';
+import './BookList.css';
 
-//https://covers.openlibrary.org/b/id/240727-S.jpg
+// https://covers.openlibrary.org/b/id/240727-S.jpg
 
 const BookList = () => {
-  const {books, loading, resultTitle} = useGlobalContext();
+  const { books, loading, resultTitle } = useGlobalContext();
   const booksWithCovers = books.map((singleBook) => {
     return {
       ...singleBook,
       // removing /works/ to get only id
       id: (singleBook.id).replace("/works/", ""),
       cover_img: singleBook.cover_id ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg` : coverImg
-    }
+    };
   });
 
-  if(loading) return <Loading />;
+  if (loading) return <Loading />;
 
   return (
-    <section className='booklist'>
+    <section className='book-details'>
       <div className='container'>
-        <div className='section-title'>
-          <h2>{resultTitle}</h2>
-        </div>
-        <div className='booklist-content grid'>
-          {
-            booksWithCovers.slice(0, 50).map((item, index) => {
-              return (
-                <Book key = {index} {...item} />
-              )
-            })
-          }
-        </div>
+        <Link to="/" className='flex flex-c back-btn'>
+          <FaArrowLeft size={22} />
+          <span className='fs-18 fw-6'>Retour à l'accueil</span>
+        </Link>
+        <section className='booklist'>
+          <div className='container'>
+            <div className='section-title'>
+              <h2>{resultTitle}</h2>
+            </div>
+            <div className='booklist-content grid'>
+              {
+                booksWithCovers.slice(0, 50).map((item, index) => {
+                  return (
+                    <Book key={index} {...item} />
+                  );
+                })
+              }
+            </div>
+          </div>
+        </section>
       </div>
     </section>
-  )
+  );
 }
 
-export default BookList
+export default BookList;
--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,15 +1,16 @@
-import React, {useRef, useEffect} from 'react';
-import {FaSearch} from "react-icons/fa";
+import React, { useRef, useEffect } from 'react';
+import { FaSearch } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../context.';
 import "./SearchForm.css";
 
 const SearchForm = () => {
-  const {setSearchTerm, setResultTitle} = useGlobalContext();
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
   const searchText = useRef('');
   const navigate = useNavigate();
 
   useEffect(() => searchText.current.focus(), []);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     let tempSearchTerm = searchText.current.value.trim();
@@ -19,7 +20,6 @@ const SearchForm = () => {
     } else {
       setSearchTerm(searchText.current.value);
     }
-
     navigate("/book");
   };
 
@@ -29,16 +29,16 @@ const SearchForm = () => {
         <div className='search-form-content'>
           <form className='search-form' onSubmit={handleSubmit}>
             <div className='search-form-elem flex flex-sb bg-white'>
-              <input type = "text" className='form-control' placeholder='The Lost World ...' ref = {searchText} />
-              <button type = "submit" className='flex flex-c' onClick={handleSubmit}>
-                <FaSearch className='text-purple' size = {32} />
+              <input type="text" className='form-control' placeholder='The Lost World ...' ref={searchText} />
+              <button type="submit" className='flex flex-c' onClick={handleSubmit}>
+                <FaSearch className='text-brown' size={32} />
               </button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SearchForm
+export default SearchForm;
